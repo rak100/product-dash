98496bfbbb51e9dfba5c6572f11694d2719353f5
rak10 <ahmadus10@icloud.com>
2023-04-20 02:59:08 +0500

    worked on extended login class

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -8,3 +8,37 @@ import { Button, TextField, Link } from '@material-ui/core';
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
+
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  login = () => {
+
+    const pwd = bcrypt.hashSync(this.state.password, salt);
+
+    axios.post('http://localhost:2000/login', {
+      username: this.state.username,
+      password: pwd,
+    }).then((res) => {
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('user_id', res.data.id);
+      this.props.history.push('/dashboard');
+    }).catch((err) => {
+      if (err.response && err.response.data && err.response.data.errorMessage) {
+        swal({
+          text: err.response.data.errorMessage,
+          icon: "error",
+          type: "error"
+        });
+      }
+    });
+  }
