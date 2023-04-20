32eab54329c5c767cdbc6d2011d1271ef72e5dc6
rak10 <ahmadus10@icloud.com>
2023-04-20 18:15:19 +0500

    added login.css, register.js

---
+++ frontend/src/Register.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import swal from 'sweetalert';
+import { Button, TextField, Link } from '@material-ui/core';
+const axios = require('axios');
+
+//cont from: class register
