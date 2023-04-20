fe8712736c341656c8078a69f935ac4a2ce94ade
rak10 <ahmadus10@icloud.com>
2023-04-21 02:50:17 +0500

    added product post feature in register.js file

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -4,3 +4,37 @@ import { Button, TextField, Link } from '@material-ui/core';
 const axios = require('axios');
 
 //cont from: class register
+export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      confirm_password: ''
+    };
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  register = () => {
+
+    axios.post('http://localhost:2000/register', {
+      username: this.state.username,
+      password: this.state.password,
+    }).then((res) => {
+      swal({
+        text: res.data.title,
+        icon: "success",
+        type: "success"
+      });
+      this.props.history.push('/');
+    }).catch((err) => {
+      swal({
+        text: err.response.data.errorMessage,
+        icon: "error",
+        type: "error"
+      });
+    });
+  }
+  //cont from render () funct
+  
