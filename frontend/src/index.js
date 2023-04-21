445c836f22ba68f9754d02c000bcfac3a29c2f75
rak10 <ahmadus10@icloud.com>
2023-04-21 15:20:20 +0500

    cleared the error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Switch, Redirect, Route } from "react-router";
+// import { Routes, Redirect, Route } from "react-router";
 import { BrowserRouter, Link } from "react-router-dom";
 
 import Login from "./Login";
@@ -19,3 +20,18 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+
+// changed to this to check if its working or not? 
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route exact path="/" component={Login} />
+//       <Route exact path="/register" component={Register} />
+//       <Route path="/dashboard" component={Dashboard} />
+//       {/* <Route component={NotFound}/> */}
+//     </Routes>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
